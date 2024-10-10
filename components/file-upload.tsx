60b0dcd720ba8f65fcff7dfd3cefb486f511148c
Hexvel <dimaabdukarimov1234@gmail.com>
2024-10-10 13:52:55 +0600

    delete server image

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,7 @@
 
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps {
@@ -17,6 +18,13 @@ const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
     return (
       <div className="relative h-20 w-20">
         <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     );
   }
